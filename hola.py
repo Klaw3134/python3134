ab1d33c65747945f8544462d0ca670d6a4847d3e
Klaw3134 <cgnavarrete3134@gmail.com>
2025-04-24 15:35:19 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,58 @@
+# print("ingrese un numeros")
+# numero=int(input())
+# print("ingrese un segundo numero")
+# numero2=int(input())
+# print("hola la suma de los numeros es", numero+numero2)
+
+# print("hola", nombre, "su edad es", edad)
+
+# if edad>=18:
+#     print("usted es mayor de edad")
+# else
+#     print("usted no es mayor de edad")
+# nombre=input
+
+# print("ingrese su edad")
+# edad=int(input())
+# if edad<12:
+#     print("usted es niño")
+# elif edad>=12 and edad<=17 :
+#     print("usted adolescente")
+# elif edad>=18 and edad<=64 :
+#     print("usted adulto")
+# else:
+#     print("usted es adulto mayor")    
+
+# ##Clave Secreta
+# clave=3134
+# print("ingrese su clave")
+# pas=int(input())
+# if pas==clave :
+#     print("Clave exitosa")
+# else:
+#     print("clave invalida")
+
+# #numeros pares e impares
+
+# num=int(input("ingrese un numero"))
+
+# if num % 2==0 :
+#     print("el numero es par")
+# else:
+#     print("el numero es impar")
+
+
+color=input("ingrese un color")
+# si es azul es de la U
+# si es blanco cc
+# si es rojo union
+# si es verde audax
+# si es otro no es color valido
+if color =="azul":
+    print("es de la U")
+elif color=="blanco":
+    print(" es de colocolo") 
+elif color=="verde":
+    print("es del audax")       
+else: 
+    print("no es un color valido")    
