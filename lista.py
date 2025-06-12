4148f3c28b25a42cdbbcde729cb96ae0c8c895d0
klaw3134 <cgnavarrete3134@gmail.com>
2025-06-12 15:23:24 -0400

    listas

---
+++ lista.py
@@ -0,0 +1,75 @@
+# uso y ejemplos de numeross
+#### controlF para remplazar palabras
+        #    -4  -3  -2   -1
+# numeros=[10, 5, 20, 45, ]
+        #    0   1  2   3
+# print(numeros [-1])
+# numeros.reverse()
+# for numero in numeros:
+#     print("numero" , numero*3)
+
+# numeros.append(64)    #(agrega un dato al final de la lista)
+
+
+# frutas=["durazno", "naranja", "manzana", "pera"]
+
+
+# for fruta in frutas:
+#     print(fruta)
+# apellidos=["navarrete", "canales", "gonzalez"]
+# nombres=["claudia", "erika", "cata"]
+# while True:
+#     print('''  
+#            1.- Ingresa un nombre y apellido
+#           2.- buscar nombres
+#           3.- mostrar nombres y apellidos
+#           4.- salir 
+#           ''')
+#     op=int(input("seleccione una opcion"))
+#     match op:
+#         case 1:
+#             p1=input("ingrese un nombre")
+#             p2=input("ingrese un apellido")
+#             nombres.append(p1)
+#             apellidos.append(p2)
+            
+#         case 2:
+#             nom=input("buscar nombre")
+#             if nom in nombres:
+#                 print("se encontró nombre")
+#             else:
+#                 print("no existe")    
+            
+#         case 3:
+#             # nomb=0
+#             # for n in nombres:
+#             #     print(nombres[nomb], apellidos[nomb])
+#             #     nomb+=1
+#             for i in range(len[nombres]):
+#                 print(nombres[i], apellidos[i])
+
+
+#         case 4:  
+#             print("salir")
+#             break
+#         case _:
+#             print("")      
+
+###### Seleccione una opcion
+# 1.- Agregar productos (nombre del producto y precio)
+# 2.- comprar (submenu mostrando productos y precios)
+# 3.- crear boleta
+# 4.- Salir
+
+
+productos=[]
+precios=[]
+carrito=[]
+
+while True:
+    print(''' 
+''')
+
+
+
+
